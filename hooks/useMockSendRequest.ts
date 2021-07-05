2e12caa002f3637c5bd45094feeaa3b428dca644
Luis Grases <luisgrases@gmail.com>
2021-07-05 11:49:40 -0600

    Add path aliases

---
+++ hooks/useMockSendRequest.ts
@@ -0,0 +1,52 @@
+import { useState, useCallback } from "react";
+
+/**
+ * Hook that mocks an API request. Useful for when building a view where the API is not ready yet.
+ */
+
+interface UseMockSendRequest {
+  (mockResponse: any, err?: any): [
+    () => void,
+    // We don't have a structure for api error objects yet
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    { isLoading: boolean; error: object; response: any }
+  ];
+}
+
+export const useMockSendRequest: UseMockSendRequest = (mockResponse, err?) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+  const [response, setResponse] = useState<any>(null);
+
+  const send = useCallback(async () => {
+    console.warn(
+      "%c WARNING! %cYou are using a mocked API request. Please remember to use the real implementation before this code goes to production. ",
+      "background: #222; color: #bada55; font-size: large;",
+      "background: #222; color: #bada55;"
+    );
+    try {
+      setIsLoading(true);
+      setError(null);
+      setResponse(null);
+      const res = await fakeApiRequest(mockResponse, err);
+      setResponse(res);
+      setError(null);
+    } catch (e) {
+      setError(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return [send, { response, isLoading, error }];
+};
+
+const fakeApiRequest = (response: any, err: any) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (err) {
+        reject(err);
+      }
+      return resolve(response);
+    }, 2000);
+  });
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,57 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect } from "react";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "components/EditScreenInfo";
+import { useMockSendRequest } from "hooks/useMockSendRequest";
+
+import { Text, View } from "components/Themed";
 
 export default function TabOneScreen() {
+  const [send, { response, error }] = useMockSendRequest([
+    { id: 1, username: "juangra" },
+    { id: 2, username: "pedrogra" },
+  ]);
+
+  useEffect(() => {
+    send();
+  }, []);
+
+  const renderItem = ({ item }: { item: { id: number; username: string } }) => (
+    <Item title={item.username} />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={response}
+        renderItem={renderItem}
+        keyExtractor={(item) => {
+          return String(item.id);
+        }}
+      />
+    </SafeAreaView>
   );
 }
+
+const Item = ({ title }: { title: string }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
